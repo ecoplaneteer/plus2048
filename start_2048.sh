7e4e6e8d53a0630df8317b8aa42605bdb418e88b
ecoplaneteer <56935694+ecoplaneteer@users.noreply.github.com>
2020-12-17 10:45:38 +0800

    Update start_2048.sh

--- start_2048.sh
+++ start_2048.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-cd frontend/plus2048 && yarn serve & cd backend/plus2048 && mix deps.get && mix ecto.setup && mix phx.gen.context Messages Message messages user:string body:string && mix ecto.migrate && mix phx.server
+cd frontend/plus2048 && yarn && yarn serve & cd backend/plus2048 && mix deps.get && mix ecto.setup && mix phx.gen.context Messages Message messages user:string body:string && mix ecto.migrate && mix phx.server
